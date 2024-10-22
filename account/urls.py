0a37cc49ff0bb683be7d5c458677849433379d44
sarahwonago <sarahshaaleh@gmail.com>
2024-10-22 17:57:12 +0300

    change password viewset

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .views import TenantViewSet
+from .views import TenantViewSet, ChangePasswordViewset
 
 router = DefaultRouter()
 router.register(r"tenants", TenantViewSet, basename="tenant")
+router.register(r"change-password", ChangePasswordViewset, basename="change-password")
 
 urlpatterns = router.urls
--- account/views.py
+++ account/views.py
@@ -20,7 +20,7 @@ class TenantViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.filter(role=User.TENANT)
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated, IsLandLord]
+    #permission_classes = [IsAuthenticated, IsLandLord]
 
     def create(self, request, *args, **kwargs):
         """
@@ -32,3 +32,31 @@ class TenantViewSet(viewsets.ModelViewSet):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+
+class ChangePasswordViewset(viewsets.ViewSet):
+    """
+    Viewset for tenants to change their passwords.
+
+    Only users with the role of tenant can change their passwords.
+    """
+
+    #permission_classes = [IsAuthenticated, IsTenant]
+
+
+    def create(self, request, *args, **kwargs):
+        """
+        Change the password of the authenticated user.
+        """
+        serializer = ChangePasswordSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        # checks if the old password matches
+        user = request.user
+        if not check_password(serializer.validated_data.get("old_password"), user.password):
+            return Response({"old_password":"Wrong password."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        # set the new password and save the user.
+        user.set_password(serializer.validated_data.get("new_password"))
+        user.save()
+
+        return Response({"message":"Password changed successfully."}, status=status.HTTP_200_OK)
